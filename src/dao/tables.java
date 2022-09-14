1e4406b6c508f212ab60d6f970ea603780f9a32e
RashmiNirasha <nirashagunawardana9@gmail.com>
2022-09-15 02:00:37 +0530

    login added

--- src/dao/tables.java
+++ src/dao/tables.java
@@ -6,15 +6,14 @@ package dao;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author 94773
- */
+
 public class tables {
     public static void main(String[] args){
         try{
         String userTable = "create table user(id int AUTO_INCREMENT primary key,name varchar(200),email varchar(200),mobileNumber varchar(10),Address varchar(200),password varchar(200),securityQuestion varchar(200),answer varchar(200),status varchar(20),UNIQUE (email))";
+       String adminDetails = "insert into user(name,email,mobileNumber,address,password,securityQuestion,answer,status) values('Admin','admin@gmail.com','0123456789','India','admin','what primary school did you attend?','ABC','true')";
         Dboperations.setDataOrDelete(userTable,"User Table Created Successfully");
+        Dboperations.setDataOrDelete(adminDetails,"Admin details added Successfully");
         }
         catch(Exception e){
             JOptionPane.showMessageDialog(null, e);
