06826b8b0e39faf8355410ebfca6b73d9b396cd9
Namadee Shakya <65288221+namadee@users.noreply.github.com>
2022-09-15 16:34:13 +0530

    Added Manage Category

--- src/dao/CategoryDao.java
+++ src/dao/CategoryDao.java
@@ -3,39 +3,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
-import java.util.ArrayList;
-import javax.swing.JOptionPane;
 import model.Category;
+import javax.swing.JOptionPane;
 import java.sql.*;
+import java.util.ArrayList;
 
+/**
+ *
+ * @author Namadee Shakya
+ */
 public class CategoryDao {
     
     public static void save(Category category){
-        String query = "insert into category(name) values ('"+category.getName()+"')";
-        Dboperations.setDataOrDelete(query,"Category added succesfully");
+        
+        String query = "insert into category (name) value('"+category.getName()+"')";
+        Dboperations.setDataOrDelete(query,"Category Added Successfully");
     }
     
-    
-    public static ArrayList<Category> getAllRecord(){
+    public static ArrayList<Category> getAllRecords(){
         ArrayList<Category> arrayList = new ArrayList<>();
         try{
-             ResultSet rs = Dboperations.getData("Select * from category");
-             while(rs.next()){
-                 Category category = new Category();
-                 category.setId(rs.getInt("id"));
-                 category.setName(rs.getString("name"));
-                 arrayList.add(category);
-             }
-             
+            ResultSet rs = Dboperations.getData("select *from category");
+            while(rs.next()){
+                Category category = new Category();
+                category.setId(rs.getInt("id"));
+                category.setName(rs.getString("name"));
+                arrayList.add(category);
+            }
         }
         catch(Exception e){
             JOptionPane.showMessageDialog(null, e);
         }
+        
         return arrayList;
     }
     
     public static void delete(String id){
-        String query = "delete from category where id = '"+id+"' ";
-                Dboperations.setDataOrDelete(query,"Category Deleted succesfully");
+        String query = "delete from category where id='"+id+"'";
+        Dboperations.setDataOrDelete(query,"Category Deleted Successfully");
     }
 }
